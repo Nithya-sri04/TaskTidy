28be5c93d64ab10020154c61021087e2a1ab9391
Nithya-sri04 <nithyasrisivakumar30@gmail.com>
2025-03-03 13:10:24 +0530

    Removed secrets from .env.local

---
+++ my-app/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,55 @@
+import NextAuth from "next-auth/next";
+import type { NextAuthOptions } from "next-auth";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    {
+      id: "descope",
+      name: "Descope",
+      type: "oauth",
+      wellKnown: `https://api.descope.com/${process.env.NEXT_PUBLIC_DESCOPE_PROJECT_ID}/.well-known/openid-configuration`,
+      authorization: { 
+        params: { 
+          scope: "openid email profile",
+          prompt: "login"
+        }
+      },
+      idToken: true,
+      clientId: process.env.DESCOPE_CLIENT_ID,
+      clientSecret: process.env.DESCOPE_CLIENT_SECRET,
+      checks: ["pkce", "state"],
+      profile(profile) {
+        return {
+          id: profile.sub,
+          name: profile.name,
+          email: profile.email,
+          image: profile.picture,
+        };
+      },
+    },
+    {
+      id: "google",
+      name: "Google",
+      type: "oauth",
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          scope: "openid profile email",
+        },
+      },
+      profile(profile) {
+        return {
+          id: profile.sub,
+          name: profile.name,
+          email: profile.email,
+          image: profile.picture,
+        };
+      },
+    },
+  ],
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
